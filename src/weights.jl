7b916047154b99746c6ab16aad938c2d9fb0db33
keckstein <korbinian90@gmail.com>
2020-01-07 12:07:37 +0100

    refactored weights -> high speedup

--- src/weights.jl
+++ src/weights.jl
@@ -1,24 +1,31 @@
 # each edge gets a weight
-function calculateweights(wrapped, nbins, weights; keyargs...)
+function calculateweights(wrapped, nbins=256, weights=:romeo; keyargs...)
     if weights == :romeo
-        return calculateweights(wrapped, nbins; keyargs...)
+        return calculateweights_romeo(wrapped, nbins; keyargs...)
     elseif weights == :bestpath
         return calculateweights_bestpath(wrapped, nbins; keyargs...)
     end
 end
 
-## ROMEO weights
-
-function calculateweights(wrapped, nbins; keyargs...)
-    mask, keyargs = parsekeyargs(keyargs, wrapped)
+function calculateweights_romeo(wrapped, nbins; keyargs...)
+    mask, P2, TEs, M, maxmag = parsekeyargs(keyargs, wrapped)
     stridelist = strides(wrapped)
     weights = zeros(UInt8, 3, size(wrapped)...)
+    # rescale
+    ## from: 1 is best and 0 worst
+    ## to: 1 is best, nbins is worst, 0 is not valid (not added to queue)
+    rescale(w) = if 0 ≤ w ≤ 1
+            max(round(Int, (1 - w) * (nbins - 1)), 1)
+        else 0
+        end
 
     for dim in 1:3
         neighbor = stridelist[dim]
         for I in LinearIndices(wrapped)
-            if mask[I]
-                weights[dim + (I-1)*3] = getweight(wrapped, I, neighbor, nbins, dim; keyargs...)
+            J = I + neighbor
+            if mask[I] && checkbounds(Bool, wrapped, J)
+                w = getweight(wrapped, I, J, P2, TEs, M, maxmag)
+                weights[dim + (I-1)*3] = rescale(w)
             end
         end
     end
@@ -26,58 +33,48 @@ function calculateweights(wrapped, nbins; keyargs...)
 end
 
 function parsekeyargs(keyargs, wrapped)
-    if haskey(keyargs, :mag)
-        args = Dict{Symbol, Any}(keyargs)
-        if haskey(keyargs, :mask)
-            @show "here"
-            args[:mag] = Float32.(args[:mag]) .* args[:mask]
+    getval(key) = if haskey(keyargs, key) keyargs[key] else nothing end
+
+    mag = getval(:mag)
+    mask = getval(:mask)
+
+    if mask != nothing
+        if mag != nothing
+            mag *= mask
         end
-        args[:maxmag] = maximum(args[:mag])
-        keyargs = NamedTuple{Tuple(keys(args))}(values(args))
-    end
-    mask = if haskey(keyargs, :mask)
-        keyargs[:mask]
     else
-        trues(size(wrapped))
+        mask = trues(size(wrapped))
     end
-    return mask, keyargs
+
+    maxmag = if mag != nothing
+        maximum(mag) else nothing
+    end
+
+    return mask, getval(:phase2), getval(:TEs), mag, maxmag
 end
 
-@inline function getweight(P, i, k, nbins, dim; keyargs...) # Phase, index, neighbor-offset, nbins, dim
-    j = i+k
-    if !checkbounds(Bool, P, j) return 0 end
+phasecoherence(P, i, j) = 1 - abs(γ(P[i] - P[j]) / π)
+phasegradientcoherence(P, P2, TEs, i, j) = max(0, 1 - abs(γ(P[i] - P[j]) - γ(P2[i] - P2[j]) * TEs[1] / TEs[2]))
+magcoherence(small, big) = (small / big) ^ 2
+magweight(small, max) = 0.5 + 0.5min(1, small / (0.5 * max))
+magweight2(big, max) = 0.5 + 0.5min(1, (0.5 * max) / big) # too high magnitude is not good either (flow artifact)
 
-    phasecoherence = 1 - abs(γ(P[i] - P[j]) / π)
+# calculates weight of one edge
+function getweight(P, i, j, P2, TEs, M, maxmag) # Phase, index, neighbor
+    weight = phasecoherence(P, i, j)
 
-    phasegradientcoherence = 1
-    if haskey(keyargs, :phase2)
-        P2, TEs = keyargs[:phase2], keyargs[:TEs]
-        phasegradientcoherence = max(0, 1 - abs(γ(P[i] - P[j]) - γ(P2[i] - P2[j]) * TEs[1] / TEs[2]))
+    if P2 != nothing && TEs != nothing
+        weight *= phasegradientcoherence(P, P2, TEs, i, j)
     end
 
-    weight = phasecoherence * phasegradientcoherence
-
-    if haskey(keyargs, :mag)
-        M, globalmaxmag = keyargs[:mag], keyargs[:maxmag]
-        mini, maxi = minmax(M[i], M[j])
-        magcoherence = (mini / maxi) ^ 2
-
-        magweight = 0.5 + 0.5min(1, mini / (0.5 * globalmaxmag))
-        magweight2 = 0.5 + 0.5min(1, (0.5 * globalmaxmag) / maxi) # too high magnitude is not good either (flow artifact)
-
-        weight *= magcoherence * magweight * magweight2
+    if M != nothing && maxmag != nothing
+        small, big = minmax(M[i], M[j])
+        weight *= magcoherence(small,big) * magweight(small,max) * magweight2(big,max)
     end
 
-    # weight: 1 is best and 0 worst
-    # resaled: 1 is best, nbins is worst, 0 is not valid (not added to queue)
-    rescale(w) = max(round(Int, (1 - w) * (nbins - 1)), 1)
-
-    if 0 ≤ weight ≤ 1
-        return rescale(weight)
-    else
-        return 0
-    end
+    return weight
 end
+
 
 ## best path weights
 # Abdul-Rahamn https://doi.org/10.1364/AO.46.006623
