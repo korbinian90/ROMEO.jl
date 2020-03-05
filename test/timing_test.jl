191ca3956d3d2f715e78a631315ea484c2262f41
keckstein <korbinian90@gmail.com>
2020-03-05 13:54:52 +0100

    fix path

--- test/timing_test.jl
+++ test/timing_test.jl
@@ -1,7 +1,7 @@
 using MRI
 
-phasefile = raw"F:\MRI\scanner_nifti\Paper\SWI_paper_7T_volunteers\19950113MTGU_201907041700\nifti\20\reform\Image.nii"
-magfile = raw"F:\MRI\scanner_nifti\Paper\SWI_paper_7T_volunteers\19950113MTGU_201907041700\nifti\19\reform\Image.nii"
+phasefile = raw"F:\MRI\scanner_nifti\Paper\SWI_paper_7T_volunteers\paper1\nifti\20\reform\Image.nii"
+magfile = raw"F:\MRI\scanner_nifti\Paper\SWI_paper_7T_volunteers\paper1\nifti\19\reform\Image.nii"
 phaseni = readphase(phasefile)
 magni = readmag(magfile)
 
