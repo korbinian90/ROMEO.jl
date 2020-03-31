18f24ddc91f3683f5e4138ac3e9f42a743657f72
keckstein <korbinian90@gmail.com>
2020-03-31 14:07:44 +0200

    changing visited to uint8

--- src/algorithm.jl
+++ src/algorithm.jl
@@ -1,15 +1,17 @@
 function growRegionUnwrap!(wrapped, weights, seed, nbins)
     stridelist = strides(wrapped)
-    visited = falses(size(wrapped))
-    notvisited(i) = checkbounds(Bool, visited, i) && !visited[i]
+    visited = zeros(UInt8, size(wrapped))
+    notvisited(i) = checkbounds(Bool, visited, i) && (visited[i] == 0)
     pqueue = PQueue(nbins, seed)
+
+    visited[getfirstvoxfromedge(seed)] = 1
 
     while !isempty(pqueue)
         edge = pop!(pqueue)
         oldvox, newvox = getvoxelsfromedge(edge, visited, stridelist)
-        if !visited[newvox]
+        if visited[newvox] == 0
             unwrapedge!(wrapped, oldvox, newvox)
-            visited[newvox] = true
+            visited[newvox] = visited[oldvox]
             for i in 1:6 # 6 directions
                 e = getnewedge(newvox, notvisited, stridelist, i)
                 if e != 0 && weights[e] > 0
@@ -21,11 +23,20 @@ function growRegionUnwrap!(wrapped, weights, seed, nbins)
     return wrapped
 end
 
+function getnewseed!(wrapped, weights, visited; keyargs...)
+    seed = findseed(wrapped, weights, visited)
+    if haskey(keyargs, :phase2) && haskey(keyargs, :TEs) # requires multiecho
+        seedcorrection!(wrapped, seed, keyargs[:phase2], keyargs[:TEs])
+    end
+    visited
+    return seed
+end
+
 function getvoxelsfromedge(edge, visited, stridelist)
     dim = getdimfromedge(edge)
     vox = getfirstvoxfromedge(edge)
     neighbor = vox + stridelist[dim] # direct neigbor in dim
-    if !visited[neighbor]
+    if visited[neighbor] == 0
         return vox, neighbor
     else
         return neighbor, vox
