884151218ba5cd220eb5d3009496a28bb0f23da7
korbi <korbinian90@gmail.com>
2020-05-09 12:10:20 +0200

    change weight tests according to changed weights

--- test/specialcases.jl
+++ test/specialcases.jl
@@ -11,9 +11,8 @@ function weight_test(w, out)
     w = reshape(w, length(w), 1, 1)
     @test UInt8.(out) == ROMEO.calculateweights(w)[1,:,1,1]
 end
-@test UInt[8, 8, 8, 0] == ROMEO.calculateweights(reshape([0.1, 0.2 + 2pi, 0.3, 0.4], 4,1,1))[1,:,1,1]
-weight_test([0.1, 0.2 + 2pi, 0.3, 0.4], [8, 8, 8, 0])
-weight_test([0.1, 0.2 + 2pi, 0.3, NaN], [8, 255, 0, 0]) # 255 (worst valid value) at voxel bordering to NaN voxel
+weight_test([0.1, 0.2 + 2pi, 0.3, 0.4], [230, 8, 230, 0]) # phase linearity bad (230) at borders
+weight_test([0.1, 0.2 + 2pi, 0.3, NaN], [230, 255, 0, 0]) # 255 to voxel bordering to NaN voxel, 0 to NaN voxel
 
 ## NaN test
 @test nan_test(unwrap([0.1, 0.2 + 2pi, 0.3, 0.4]), [0.1, 0.2, 0.3, 0.4])
