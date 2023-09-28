6ec680603003e2f92901c76a28708f0825eae281
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-09-29 07:28:39 +1000

    Update romeo.jl
    Install packages automatically

--- romeo.jl
+++ romeo.jl
@@ -1,6 +1,11 @@
 import Pkg
 Pkg.activate(@__DIR__)
-using ROMEO, MriResearchTools, ArgParse
+try
+    using ROMEO, MriResearchTools, ArgParse
+catch
+    Pkg.add(["ROMEO", "MriResearchTools", "ArgParse"])
+    using ROMEO, MriResearchTools, ArgParse
+end
 
 @time msg = unwrapping_main(ARGS)
 println(msg)
